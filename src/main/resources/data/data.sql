40521d0b5528b246f7a349b88c8dd9f73b318fc8
jordi <rauxasoft@gmail.com>
2023-04-28 12:39:20 +0200

    listo para crear tablas

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO GRUPOS_MUSICALES (CODIGO, NOMBRE) VALUES
+(1, 'Funkadelic'),
+(2, 'Parliament'),
+(3, 'Sly And The Family Stone'),
+(4, 'Golpes Bajos');
+
+/*
+ * TODO Insertar datos de ejemplo para todas las tablas creadas en el schema.sql
+ * 
+ */
---
+++ src/main/resources/data/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE GRUPOS_MUSICALES(
+
+	CODIGO		INTEGER		      	NOT NULL,
+	NOMBRE		VARCHAR(200)  		,
+	
+	PRIMARY KEY (CODIGO)
+
+);
+
+
+/*
+ * TODO Crear las tablas
+ * 
+ * 1. Categorias
+ * 2. Camareros
+ * 3. Productos
+ * 4. Pedidos
+ * 5. Lineas_Pedido
+ * 
+ */
